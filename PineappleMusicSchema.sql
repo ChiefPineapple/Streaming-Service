0cb90e4b8e4e9993205f1f2c22a4eb4ad68e52a7
Alexander Fleming <amfleming3861@gmail.com>
2019-03-13 20:15:52 -0400

    Add files via upload

--- PineappleMusicSchema.sql
+++ PineappleMusicSchema.sql
@@ -3,41 +3,41 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema PineappleMusicSchema
+-- Schema PineappleMusic
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `PineappleMusicSchema` ;
+DROP SCHEMA IF EXISTS `PineappleMusic` ;
 
 -- -----------------------------------------------------
--- Schema PineappleMusicSchema
+-- Schema PineappleMusic
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `PineappleMusicSchema` DEFAULT CHARACTER SET utf8 ;
-USE `PineappleMusicSchema` ;
+CREATE SCHEMA IF NOT EXISTS `PineappleMusic` DEFAULT CHARACTER SET utf8 ;
+USE `PineappleMusic` ;
 
 -- -----------------------------------------------------
--- Table `PineappleMusicSchema`.`Accounts`
+-- Table `PineappleMusic`.`Accounts`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusicSchema`.`Accounts` ;
+DROP TABLE IF EXISTS `PineappleMusic`.`Accounts` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusicSchema`.`Accounts` (
-  `accountID` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `PineappleMusic`.`Accounts` (
+  `accountID` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `passwordResetCode` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `location` VARCHAR(45) NULL,
   PRIMARY KEY (`accountID`),
-  UNIQUE INDEX `accountID_UNIQUE` (`accountID` ASC) VISIBLE,
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+  UNIQUE INDEX `accountID_UNIQUE` (`accountID` ASC)  ,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC)  ,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC)  )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PineappleMusicSchema`.`Artist`
+-- Table `PineappleMusic`.`Artist`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusicSchema`.`Artist` ;
+DROP TABLE IF EXISTS `PineappleMusic`.`Artist` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusicSchema`.`Artist` (
+CREATE TABLE IF NOT EXISTS `PineappleMusic`.`Artist` (
   `acntID` INT NOT NULL,
   `stageName` VARCHAR(45) NOT NULL,
   `location` VARCHAR(45) NULL,
@@ -46,34 +46,34 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PineappleMusicSchema`.`AccountFollowsArtist`
+-- Table `PineappleMusic`.`AccountFollowsArtist`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusicSchema`.`AccountFollowsArtist` ;
+DROP TABLE IF EXISTS `PineappleMusic`.`AccountFollowsArtist` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusicSchema`.`AccountFollowsArtist` (
+CREATE TABLE IF NOT EXISTS `PineappleMusic`.`AccountFollowsArtist` (
   `acntID` INT NOT NULL,
   `artistID` INT NOT NULL,
   PRIMARY KEY (`acntID`, `artistID`),
-  INDEX `fk_AccountFollowsArtist_Artist1_idx` (`artistID` ASC) VISIBLE,
+  INDEX `fk_AccountFollowsArtist_Artist1_idx` (`artistID` ASC)  ,
   CONSTRAINT `fk_AccountFollowsArtist_Accounts`
     FOREIGN KEY (`acntID`)
-    REFERENCES `PineappleMusicSchema`.`Accounts` (`accountID`)
+    REFERENCES `PineappleMusic`.`Accounts` (`accountID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_AccountFollowsArtist_Artist1`
     FOREIGN KEY (`artistID`)
-    REFERENCES `PineappleMusicSchema`.`Artist` (`acntID`)
+    REFERENCES `PineappleMusic`.`Artist` (`acntID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PineappleMusicSchema`.`Songs`
+-- Table `PineappleMusic`.`Songs`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusicSchema`.`Songs` ;
+DROP TABLE IF EXISTS `PineappleMusic`.`Songs` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusicSchema`.`Songs` (
+CREATE TABLE IF NOT EXISTS `PineappleMusic`.`Songs` (
   `songID` INT NOT NULL,
   `filename` VARCHAR(45) NULL,
   PRIMARY KEY (`songID`))
@@ -81,68 +81,68 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PineappleMusicSchema`.`SongOwner`
+-- Table `PineappleMusic`.`SongOwner`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusicSchema`.`SongOwner` ;
+DROP TABLE IF EXISTS `PineappleMusic`.`SongOwner` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusicSchema`.`SongOwner` (
+CREATE TABLE IF NOT EXISTS `PineappleMusic`.`SongOwner` (
   `acntID` INT NOT NULL,
   `sID` INT NOT NULL,
   PRIMARY KEY (`acntID`, `sID`),
-  INDEX `fk_SongOwner_Songs1_idx` (`sID` ASC) VISIBLE,
+  INDEX `fk_SongOwner_Songs1_idx` (`sID` ASC)  ,
   CONSTRAINT `fk_SongOwner_Artist1`
     FOREIGN KEY (`acntID`)
-    REFERENCES `PineappleMusicSchema`.`Artist` (`acntID`)
+    REFERENCES `PineappleMusic`.`Artist` (`acntID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_SongOwner_Songs1`
     FOREIGN KEY (`sID`)
-    REFERENCES `PineappleMusicSchema`.`Songs` (`songID`)
+    REFERENCES `PineappleMusic`.`Songs` (`songID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PineappleMusicSchema`.`Playlist`
+-- Table `PineappleMusic`.`Playlist`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusicSchema`.`Playlist` ;
+DROP TABLE IF EXISTS `PineappleMusic`.`Playlist` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusicSchema`.`Playlist` (
+CREATE TABLE IF NOT EXISTS `PineappleMusic`.`Playlist` (
   `playlistID` INT NOT NULL,
   `ownerID` INT NULL,
   `name` VARCHAR(45) NULL,
   `album` TINYINT NULL COMMENT 'if playlist is an album, all of the songs in the playlist must be owned by the owner of the playlist',
   `Accounts_accountID` INT NOT NULL,
   PRIMARY KEY (`playlistID`),
-  INDEX `fk_Playlist_Accounts1_idx` (`ownerID` ASC) VISIBLE,
+  INDEX `fk_Playlist_Accounts1_idx` (`ownerID` ASC)  ,
   CONSTRAINT `fk_Playlist_Accounts1`
     FOREIGN KEY (`ownerID`)
-    REFERENCES `PineappleMusicSchema`.`Accounts` (`accountID`)
+    REFERENCES `PineappleMusic`.`Accounts` (`accountID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PineappleMusicSchema`.`Playlist_has_Songs`
+-- Table `PineappleMusic`.`Playlist_has_Songs`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusicSchema`.`Playlist_has_Songs` ;
+DROP TABLE IF EXISTS `PineappleMusic`.`Playlist_has_Songs` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusicSchema`.`Playlist_has_Songs` (
+CREATE TABLE IF NOT EXISTS `PineappleMusic`.`Playlist_has_Songs` (
   `playlistID` INT NOT NULL,
   `songID` INT NOT NULL,
   PRIMARY KEY (`playlistID`, `songID`),
-  INDEX `fk_Playlist_has_Songs_Songs1_idx` (`songID` ASC) VISIBLE,
-  INDEX `fk_Playlist_has_Songs_Playlist1_idx` (`playlistID` ASC) VISIBLE,
+  INDEX `fk_Playlist_has_Songs_Songs1_idx` (`songID` ASC)  ,
+  INDEX `fk_Playlist_has_Songs_Playlist1_idx` (`playlistID` ASC)  ,
   CONSTRAINT `fk_Playlist_has_Songs_Playlist1`
     FOREIGN KEY (`playlistID`)
-    REFERENCES `PineappleMusicSchema`.`Playlist` (`playlistID`)
+    REFERENCES `PineappleMusic`.`Playlist` (`playlistID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Playlist_has_Songs_Songs1`
     FOREIGN KEY (`songID`)
-    REFERENCES `PineappleMusicSchema`.`Songs` (`songID`)
+    REFERENCES `PineappleMusic`.`Songs` (`songID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
