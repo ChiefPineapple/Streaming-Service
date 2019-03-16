e59888bb8264d6df91c28f35175fd326d83f90a5
Alexander Fleming <amfleming3861@gmail.com>
2019-03-16 12:21:39 -0400

    Add files via upload

--- PineappleMusicSchema.sql
+++ PineappleMusicSchema.sql
@@ -3,22 +3,22 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema PineappleMusic
+-- Schema pineapplemusic
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `PineappleMusic` ;
+DROP SCHEMA IF EXISTS `pineapplemusic` ;
 
 -- -----------------------------------------------------
--- Schema PineappleMusic
+-- Schema pineapplemusic
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `PineappleMusic` DEFAULT CHARACTER SET utf8 ;
-USE `PineappleMusic` ;
+CREATE SCHEMA IF NOT EXISTS `pineapplemusic` DEFAULT CHARACTER SET utf8 ;
+USE `pineapplemusic` ;
 
 -- -----------------------------------------------------
--- Table `PineappleMusic`.`Accounts`
+-- Table `pineapplemusic`.`Accounts`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusic`.`Accounts` ;
+DROP TABLE IF EXISTS `pineapplemusic`.`Accounts` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusic`.`Accounts` (
+CREATE TABLE IF NOT EXISTS `pineapplemusic`.`Accounts` (
   `accountID` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
@@ -33,103 +33,105 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PineappleMusic`.`Artist`
+-- Table `pineapplemusic`.`Artist`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusic`.`Artist` ;
+DROP TABLE IF EXISTS `pineapplemusic`.`Artist` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusic`.`Artist` (
+CREATE TABLE IF NOT EXISTS `pineapplemusic`.`Artist` (
   `acntID` INT NOT NULL,
   `stageName` VARCHAR(45) NOT NULL,
   `location` VARCHAR(45) NULL,
+  `artistTag` VARCHAR(45) NULL,
   PRIMARY KEY (`acntID`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PineappleMusic`.`AccountFollowsArtist`
+-- Table `pineapplemusic`.`AccountFollowsArtist`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusic`.`AccountFollowsArtist` ;
+DROP TABLE IF EXISTS `pineapplemusic`.`AccountFollowsArtist` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusic`.`AccountFollowsArtist` (
+CREATE TABLE IF NOT EXISTS `pineapplemusic`.`AccountFollowsArtist` (
   `acntID` INT NOT NULL,
   `artistID` INT NOT NULL,
   PRIMARY KEY (`acntID`, `artistID`),
   INDEX `fk_AccountFollowsArtist_Artist1_idx` (`artistID` ASC)  ,
   CONSTRAINT `fk_AccountFollowsArtist_Accounts`
     FOREIGN KEY (`acntID`)
-    REFERENCES `PineappleMusic`.`Accounts` (`accountID`)
+    REFERENCES `pineapplemusic`.`Accounts` (`accountID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_AccountFollowsArtist_Artist1`
     FOREIGN KEY (`artistID`)
-    REFERENCES `PineappleMusic`.`Artist` (`acntID`)
+    REFERENCES `pineapplemusic`.`Artist` (`acntID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PineappleMusic`.`Songs`
+-- Table `pineapplemusic`.`Songs`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusic`.`Songs` ;
+DROP TABLE IF EXISTS `pineapplemusic`.`Songs` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusic`.`Songs` (
-  `songID` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `pineapplemusic`.`Songs` (
+  `songID` INT NOT NULL AUTO_INCREMENT,
   `filename` VARCHAR(45) NULL,
+  `songTag` VARCHAR(45) NULL,
   PRIMARY KEY (`songID`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PineappleMusic`.`SongOwner`
+-- Table `pineapplemusic`.`SongOwner`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusic`.`SongOwner` ;
+DROP TABLE IF EXISTS `pineapplemusic`.`SongOwner` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusic`.`SongOwner` (
+CREATE TABLE IF NOT EXISTS `pineapplemusic`.`SongOwner` (
   `acntID` INT NOT NULL,
   `sID` INT NOT NULL,
   PRIMARY KEY (`acntID`, `sID`),
   INDEX `fk_SongOwner_Songs1_idx` (`sID` ASC)  ,
   CONSTRAINT `fk_SongOwner_Artist1`
     FOREIGN KEY (`acntID`)
-    REFERENCES `PineappleMusic`.`Artist` (`acntID`)
+    REFERENCES `pineapplemusic`.`Artist` (`acntID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_SongOwner_Songs1`
     FOREIGN KEY (`sID`)
-    REFERENCES `PineappleMusic`.`Songs` (`songID`)
+    REFERENCES `pineapplemusic`.`Songs` (`songID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PineappleMusic`.`Playlist`
+-- Table `pineapplemusic`.`Playlist`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusic`.`Playlist` ;
+DROP TABLE IF EXISTS `pineapplemusic`.`Playlist` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusic`.`Playlist` (
-  `playlistID` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `pineapplemusic`.`Playlist` (
+  `playlistID` INT NOT NULL AUTO_INCREMENT,
   `ownerID` INT NULL,
   `name` VARCHAR(45) NULL,
   `album` TINYINT NULL COMMENT 'if playlist is an album, all of the songs in the playlist must be owned by the owner of the playlist',
-  `Accounts_accountID` INT NOT NULL,
+  `playlistTag` VARCHAR(45) NULL,
   PRIMARY KEY (`playlistID`),
   INDEX `fk_Playlist_Accounts1_idx` (`ownerID` ASC)  ,
   CONSTRAINT `fk_Playlist_Accounts1`
     FOREIGN KEY (`ownerID`)
-    REFERENCES `PineappleMusic`.`Accounts` (`accountID`)
+    REFERENCES `pineapplemusic`.`Accounts` (`accountID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PineappleMusic`.`Playlist_has_Songs`
+-- Table `pineapplemusic`.`Playlist_has_Songs`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PineappleMusic`.`Playlist_has_Songs` ;
+DROP TABLE IF EXISTS `pineapplemusic`.`Playlist_has_Songs` ;
 
-CREATE TABLE IF NOT EXISTS `PineappleMusic`.`Playlist_has_Songs` (
+CREATE TABLE IF NOT EXISTS `pineapplemusic`.`Playlist_has_Songs` (
   `playlistID` INT NOT NULL,
   `songID` INT NOT NULL,
   PRIMARY KEY (`playlistID`, `songID`),
@@ -137,12 +139,12 @@ CREATE TABLE IF NOT EXISTS `PineappleMusic`.`Playlist_has_Songs` (
   INDEX `fk_Playlist_has_Songs_Playlist1_idx` (`playlistID` ASC)  ,
   CONSTRAINT `fk_Playlist_has_Songs_Playlist1`
     FOREIGN KEY (`playlistID`)
-    REFERENCES `PineappleMusic`.`Playlist` (`playlistID`)
+    REFERENCES `pineapplemusic`.`Playlist` (`playlistID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Playlist_has_Songs_Songs1`
     FOREIGN KEY (`songID`)
-    REFERENCES `PineappleMusic`.`Songs` (`songID`)
+    REFERENCES `pineapplemusic`.`Songs` (`songID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
