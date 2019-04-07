26417b4dcaed47ec63626cfa68fb19181696a20d
Alexander Fleming <amfleming3861@gmail.com>
2019-04-07 17:09:18 -0400

    Add files via upload

--- nodeinterface.js
+++ nodeinterface.js
@@ -3,6 +3,7 @@ var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
+var nodemailer = require('nodemailer');
 
 var connection = mysql.createConnection({
 	host     : 'localhost',
@@ -21,6 +22,14 @@ app.use(session({
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'pineapplemusicdonotreply@gmail.com',
+		pass: 'Softwareeng!neering2019'
+	}
+});
+
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/firstPage.html'));
 });
@@ -29,8 +38,16 @@ app.get('/homePage', function(request, response) {
 	response.sendFile(path.join(__dirname + '/homePage.html'));
 });
 
-app.get('/artistHomePage', function(request, response) {
-	response.sendFile(path.join(__dirname + '/artistHomePage.html'));
+app.get('/artistProfilePage', function(request, response) {
+	response.sendFile(path.join(__dirname + '/artistProfilePage.html'));
+});
+
+app.get('/nonArtistProfilePage', function(request, response) {
+	response.sendFile(path.join(__dirname + '/nonArtistProfilePage.html'));
+});
+
+app.get('/ForgotPassword', function(request, response) {
+	response.sendFile(path.join(__dirname + '/forgotPassword.html'));
 });
 
 app.post('/loadCreateAccount', function(request, response) {
@@ -52,10 +69,11 @@ app.post('/authLogin', function(request, response) {
 				request.session.userID = results[0].accountID;
 				connection.query('SELECT * FROM artist WHERE acntID = ?', [request.session.userID], function(error, results, fields) {
 					if (results.length > 0) {
-						response.redirect('/artistHomePage');
+						request.session.isArtist = true;
 					}else{
-						response.redirect('/homePage');
+						request.session.isArtist = false;
 					}
+					response.redirect('/homePage');
 				});
 			} else {
 				response.send('Incorrect Username and/or Password!');
@@ -67,7 +85,32 @@ app.post('/authLogin', function(request, response) {
 		response.end();
 	}
 });
-// the querys in this post are nested because the output of one query 
+
+app.post('/forgotPassword', function(request,response) {
+	connection.query('SELECT passwordResetCode FROM accounts WHERE email = ?', [request.body.email], function(error, results, fields) {
+		if (results.length > 0) {
+			var mailOptions = {
+				from: 'pineapplemusicdonotreply@gmail.com',
+				to: request.body.email,
+				subject: 'Reset password for Pineapple Music',
+				text: 'your reset code is: ' + results[0].passwordResetCode
+			};
+			
+			transporter.sendMail(mailOptions, function(error, info){
+				if (error) {
+					console.log(error);
+				} else {
+					console.log('Email sent succesfully: ' + info.response);
+					response.end();
+				}
+			});
+		
+		}else{
+			response.send("There is no account associated with this email");
+		}
+	});
+}); 
+
 app.post('/authCreateAccount', function(request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
@@ -158,10 +201,18 @@ app.post('/lookup', function(request, response) {
 	//response.end();
 });
 
+app.post('/loadProfile', function(request, response) {
+	if (request.session.isArtist==true)
+		response.redirect('/artistProfilePage');
+	else
+		response.redirect('/nonArtistProfilePage');
+});
+
 app.post('/becomeArtist', function(request, response) {
 	if (request.session.loggedin) {
 		connection.query('INSERT INTO artist VALUES (?,?,?,?)', [request.session.userID, request.body.stageName, request.body.location, request.body.artistTag], function (error, results, fields) {
-			response.redirect('/artistHomePage');
+			request.session.isArtist=true;
+			response.redirect('/artistProfilePage');
 		});
 	
 	} else {
